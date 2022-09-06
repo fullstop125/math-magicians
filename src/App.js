f16fa7d1e95f5c15a871f8e5beef48ba862c17d1
Restyled.io <commits@restyled.io>
2022-09-06 22:13:45 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import './App.css';
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Math Magicians
-        </p>
-
+        <p>Math Magicians</p>
       </header>
     </div>
   );

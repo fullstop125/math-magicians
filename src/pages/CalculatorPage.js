1cdc4c8f55be000914035b71dc1625e87db51141
fullstop125 <hassanmoses125@gmail.com>
2022-09-15 16:42:58 +0300

    add react routers

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <div>
+    <h1 className="math-text">Let&apos;s do some math!</h1>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
---
+++ src/pages/Quote.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../assets/styles/quote.css';
+
+const Quote = () => (
+  <div>
+    <h1>Quote</h1>
+    <p>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. –William Paul Thurston
+
+    </p>
+    <p>
+      Pure mathematics is, in its way, the poetry of logical ideas.
+      — Albert Einstein, German theoretical physicist
+    </p>
+
+  </div>
+);
+
+export default Quote;
